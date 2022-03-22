263cb892ed9466007f39b6ddc29b1b3b2bbd4609
Ryan Chiechi <rchiech@ncsu.edu>
2022-03-22 08:04:20 -0400

    Because log.py was COL

--- log.py
+++ log.py
@@ -1,18 +1,14 @@
-# import RPi.GPIO as GPIO  # type: ignore
 import os
 import time
-from datetime import datetime
 import logging
 from util import DOOROPEN
 from util import DOORCLOSED
 from util import DOORUNKNOWN
-# from util import DOOROPENING
-# from util import DOORCLOSING
+from util import LOGFILE
 from util import getGarageDoorState
 from util import triggerWebHook
-from util import lastDoorState
+from util import getLastDoorState
 from util import door_dict
-from util import LOGFILE
 
 logFormatter = logging.Formatter("%(name)s: %(asctime)s [%(levelname)-5.5s]  %(message)s")
 logger = logging.getLogger('GarageWebLogger')
@@ -27,25 +23,18 @@ logger.setLevel(logging.INFO)
 logger.info("Hello! Program Starting.")
 print(" Control + C to exit Program")
 
-
-def gettimestring():
-    __strftime = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-    return datetime.strptime(__strftime, '%Y-%m-%d %H:%M:%S')
-
-
-TimeDoorOpened = gettimestring()  # Default Time
+TimeDoorOpened = time.now()  # Default Time
 DoorOpenTimer = 0  # Default start status turns timer off
 DoorOpenTimerMessageSent = 1  # Turn off messages until timer is started
 
-# try:
+
 while True:
     if os.path.getsize(LOGFILE) > 1024**2:
         os.unlink(LOGFILE)  # Prune log file when it hits 1 MB
-    logger.debug("Last door state was: %s", door_dict[lastDoorState()])
+    logger.debug("Last door state was: %s", door_dict[getLastDoorState()])
     time.sleep(10)
     if DoorOpenTimer == 1:  # Door Open Timer has Started
-        currentTimeDate = gettimestring()
-        if (currentTimeDate - TimeDoorOpened).seconds > 900 and DoorOpenTimerMessageSent == 0:
+        if time.time() - TimeDoorOpened > 900 and DoorOpenTimerMessageSent == 0:
             logger.info("Your Garage Door has been Open for 15 minutes")
             DoorOpenTimerMessageSent = 1
 
@@ -62,12 +51,8 @@ while True:
     if getGarageDoorState() == DOOROPEN:  # Door is Open
         logger.info("Door Open")
         # Start Door Open Timer
-        TimeDoorOpened = gettimestring()
+        TimeDoorOpened = time.time()
         DoorOpenTimer = 1
         DoorOpenTimerMessageSent = 0
         triggerWebHook('update', 'open')
         continue
-
-# except KeyboardInterrupt:
-#    logger.info('Goodbye! -- Log Program Shutdown')
-#    GPIO.cleanup()
