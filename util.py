9afd175820648761e2875aed65df973d5d1f28db
Ryan Chiechi <rchiech@ncsu.edu>
2023-10-18 11:21:07 -0400

    Warn if no pw file found

--- util.py
+++ util.py
@@ -51,6 +51,7 @@ def getPassword(fn=None):
     if fn is None:  # By default read from file "pw" in same dir as python scripts
         fn = os.path.join(os.path.dirname(os.path.realpath(__file__)), "pw")
     if not os.path.exists(fn):
+        logger.warn("Did not find pw file, returning default password.")
         return "12345678"  # If pw file does not exist, return default password
     with open(fn, 'rt') as fh:
         # Read first 128 bytes of file and return as a string
